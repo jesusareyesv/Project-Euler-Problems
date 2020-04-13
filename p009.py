773cdb270e48af374db4b458015392efb8ce0306
Jesús Alfredo Reyes Vargas <jesusareyesv.ve@gmail.com>
2020-04-13 10:16:55 -0400

    Fix p9

--- p009.py
+++ p009.py
@@ -10,12 +10,14 @@ def ismile(a, b, c):
 
 
 count = 0
+A, B, C = (0,)*3
 for a in range(1, 1001):
     for b in range(a+1, 1001):
         for c in range(b+1, 1001):
             if ismile(a, b, c):
                 if is_triangular(a, b, c):
-                    print(a, b, c)
+                    A, B, C = a, b, c
+                    break
 
-print(count)
+print(A*B*C)
 # 200 375 425
